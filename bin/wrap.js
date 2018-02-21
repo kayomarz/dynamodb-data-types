95fd95fe6d610649bf5f160bb76b5ecce042983d
Ben Neigher <bneigher@gmail.com>
2018-02-21 14:54:42 -0800

    [bin] added wrap + unwrap cli commands to quickly debug or extract data from dynamo

---
+++ bin/wrap.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const attr = require('../lib/dynamodb-data-types').AttributeValue;
+const content = process.argv[2];
+
+if (!content) {
+  throw new Error('Input data required.')
+}
+
+let payload = '';
+try {
+  payload = JSON.parse(content);
+}
+catch (err) {
+  payload = content
+}
+const output = attr.wrap(payload);
+
+if (typeof output === 'object') {
+  process.stdout.write(JSON.stringify(output) + '\n');
+}
+else {
+  process.stdout.write(output + '\n');
+}
