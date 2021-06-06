51e3ff958516f0672921aae6d8bbbef5dcfc13c9
EviSong <evisong@live.com>
2021-06-06 16:42:10 +0800

    [bin] wrap + unwrap cli support non-TTY (stdin)

--- bin/wrap.js
+++ bin/wrap.js
@@ -3,24 +3,37 @@
 'use strict';
 
 const attr = require('../lib/dynamodb-data-types').AttributeValue;
-const content = process.argv[2];
 
-if (!content) {
-  throw new Error('Input data required.')
+if (process.stdin.isTTY) {
+  wrap(process.argv[2]);
+} else {
+  let data = "";
+  process.stdin.on('data', function(chunk) {
+    data += chunk;
+  });
+  process.stdin.on('end', function() {
+    wrap(data);
+  });
 }
 
-let payload = '';
-try {
-  payload = JSON.parse(content);
-}
-catch (err) {
-  payload = content
-}
-const output = attr.wrap(payload);
+function wrap(content) {
+  if (!content) {
+    throw new Error('Input data required.')
+  }
 
-if (typeof output === 'object') {
-  process.stdout.write(JSON.stringify(output) + '\n');
-}
-else {
-  process.stdout.write(output + '\n');
+  let payload = '';
+  try {
+    payload = JSON.parse(content);
+  }
+  catch (err) {
+    payload = content
+  }
+  const output = attr.wrap(payload);
+
+  if (typeof output === 'object') {
+    process.stdout.write(JSON.stringify(output) + '\n');
+  }
+  else {
+    process.stdout.write(output + '\n');
+  }
 }
